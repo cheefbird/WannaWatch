ef3048d0e8045ba3e17561daf20edddf5a237a30
Francis Breidenbach <fbreidenbach@gmail.com>
2017-09-16 18:00:50 -0700

    Request router for paginated movies.

--- WannaWatch/API/RequestRouter.swift
+++ WannaWatch/API/RequestRouter.swift
@@ -12,4 +12,61 @@ import Alamofire
 
 enum RequestRouter: URLRequestConvertible {
   
+  
+  case getMovies(page: Int)
+  
+  
+  
+  private var apiKey: String {
+    return "7e6576c1d12633c8fd1eee0cb2e995ed"
+  }
+  
+  
+  var baseURL: URL {
+    return URL(string: "https://api.themoviedb.org/3/")!
+  }
+  
+  
+  var path: String {
+    switch self {
+    case .getMovies:
+      return "/discover/movie"
+    }
+  }
+  
+  
+  var method: HTTPMethod {
+    return .get
+  }
+  
+  
+  var parameters: Parameters {
+    var params = Parameters()
+    params["api_key"] = apiKey
+    params["sorty_by"] = "popularity.desc"
+    params["include_adult"] = false
+    params["include_video"] = false
+    
+    switch self {
+    case .getMovies(let page):
+      params["page"] = page
+    }
+    
+    return params
+    
+  }
+  
+  
+  func asURLRequest() throws -> URLRequest {
+    
+    let requestURL = baseURL.appendingPathComponent(path)
+    
+    var request = URLRequest(url: requestURL)
+    request.httpMethod = method.rawValue
+    
+    let encoding = URLEncoding.default
+    
+    return try! encoding.encode(request, with: parameters)
+    
+  }
 }
