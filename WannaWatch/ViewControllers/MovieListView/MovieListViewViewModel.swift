bb27865780baebb90f104e4484c1717352b1fc5c
Francis Breidenbach <fbreidenbach@gmail.com>
2017-09-17 00:31:50 -0700

    Forgot VM in previous commit.

---
+++ WannaWatch/ViewControllers/MovieListView/MovieListViewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MovieListViewViewModel.swift
+//  WannaWatch
+//
+//  Created by Francis Breidenbach on 9/16/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+
+
+struct MovieListViewViewModel {
+  
+  var status = "Not Ready"
+  
+}
