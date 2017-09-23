b34ebb3f6342864855f10fe6e832e6906709397d
Francis Breidenbach <fbreidenbach@gmail.com>
2017-09-23 15:04:52 -0700

    Create MovieServiceType and MovieService. Implement fetchMovies and saveMovies.

---
+++ WannaWatch/Services/MovieService/MovieService.swift
@@ -0,0 +1,78 @@
+//
+//  MovieService.swift
+//  WannaWatch
+//
+//  Created by Francis Breidenbach on 9/23/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import RxSwift
+import RxAlamofire
+import SwiftyJSON
+
+
+struct MovieService: MovieServiceType {
+  
+  
+  let disposeBag = DisposeBag()
+  
+  
+  fileprivate func withRealm<T>(_ operation: String, action: (Realm) throws -> T) -> T? {
+    do {
+      let realm = try Realm()
+      return try action(realm)
+    } catch let error {
+      print("Failed \(operation) realm with error: \(error)")
+      return nil
+    }
+  }
+  
+  
+  @discardableResult
+  func fetchMovies(forPage page: Int) -> Observable<[Movie]> {
+    var result = [Movie]()
+    
+    requestJSON(RequestRouter.getMovies(page: page))
+      .subscribe(onNext: { (response, data) in
+        guard let json = data as? [String: Any] else { return }
+        guard let movies = json["results"] as? [[String: Any]] else { return }
+        
+        let moviesJSON = movies.map { JSON($0) }
+        
+        for movie in moviesJSON {
+          let newMovie = Movie(fromJSON: movie)
+          result.append(newMovie)
+        }
+        
+      })
+      .disposed(by: disposeBag)
+    
+    saveMovies(result)
+    
+    return Observable.just(result)
+    
+  }
+  
+  
+  func movies() -> Observable<Results<Movie>> {
+    <#code#>
+  }
+  
+  
+  @discardableResult
+  func toggleFavorite(movie: Movie) -> Observable<Movie> {
+    <#code#>
+  }
+  
+  
+  func saveMovies(_ movies: [Movie]) {
+    let realm = try! Realm()
+    try! realm.write {
+      for movie in movies {
+        realm.add(movie, update: true)
+      }
+    }
+  }
+}
---
+++ WannaWatch/Services/MovieService/MovieServiceType.swift
@@ -0,0 +1,27 @@
+//
+//  MovieServiceType.swift
+//  WannaWatch
+//
+//  Created by Francis Breidenbach on 9/23/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RealmSwift
+
+
+protocol MovieServiceType {
+  
+  func movies() -> Observable<Results<Movie>>
+  
+  @discardableResult
+  func toggleFavorite(movie: Movie) -> Observable<Movie>
+  
+  @discardableResult
+  func fetchMovies(forPage page: Int) -> Observable<[Movie]>
+  
+  
+  func saveMovies(_ movies: [Movie])
+  
+}
