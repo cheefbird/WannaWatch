6821cac44a29c434494b386e1de40267618addca
Francis Breidenbach <fbreidenbach@gmail.com>
2017-09-25 22:34:33 -0700

    Finish implementing SceneCoordinator.

--- WannaWatch/SceneCoordinator/SceneCoordinator.swift
+++ WannaWatch/SceneCoordinator/SceneCoordinator.swift
@@ -12,7 +12,7 @@ import RxCocoa
 
 
 class SceneCoordinator: SceneCoordinatorType {
-
+  
   
   
   // MARK: - Properties
@@ -50,6 +50,13 @@ class SceneCoordinator: SceneCoordinatorType {
   
   // MARK: - Transitions
   
+  /// Perform scene transition. On successful transition, resulting observable completes. Can be subscribed to if you want to take further action, as it works like a completion callback.
+  ///
+  /// - Parameters:
+  ///   - scene: Destination scene for transition.
+  ///   - type: Type of transition to perform. Only use push when calling from view with UINavigationController
+  /// - Returns: Void/empty Observable that doesn't emit anything and completes when transition is completed.
+  @discardableResult
   func transition(to scene: Scene, type: SceneTransitionType) -> Observable<Void> {
     let subject = PublishSubject<Void>()
     let viewController = scene.viewController()
@@ -64,14 +71,80 @@ class SceneCoordinator: SceneCoordinatorType {
       guard let navigationController = currentViewController.navigationController else {
         fatalError("Attempted to push a view controller when no nav controller exists.")
       }
-      // 
       
+      // Intercept navigationController(_:didShow:animated:) delegate message and emit when new VC is pushed
+      _ = navigationController.rx.delegate
+        .sentMessage(#selector(UINavigationControllerDelegate.navigationController(_:didShow:animated:)))
+        .map { _ in }
+        .bind(to: subject)
+      // Push the new controller and trigger the previous observable
+      navigationController.pushViewController(viewController, animated: true)
+      currentViewController = SceneCoordinator.actualViewController(for: viewController)
+      
+    case .modal:
+      currentViewController.present(viewController, animated: true) {
+        subject.onCompleted()
+      }
+      currentViewController = SceneCoordinator.actualViewController(for: viewController)
     }
+    
+    return subject.asObservable()
+      .take(1)
+      .ignoreElements()
   }
   
   
+  /// Perform backwards transition. Dismisses modals and pops controllers from nav stack.
+  ///
+  /// - Parameter animated: Perform animation when transitioning.
+  /// - Returns: Void/empty Observable that doesn't emit anything and completes when transition is completed.
+  @discardableResult
   func pop(animated: Bool) -> Observable<Void> {
-    <#code#>
+    let subject = PublishSubject<Void>()
+    
+    // Dismiss scene presented modally.
+    if let presenter = currentViewController.presentingViewController {
+      currentViewController.dismiss(animated: animated) {
+        
+        self.currentViewController = SceneCoordinator.actualViewController(for: presenter)
+        subject.onCompleted()
+      }
+      // Pop scene from navigation stack.
+    } else if let navigationController = currentViewController.navigationController {
+      
+      _ = navigationController.rx.delegate
+        .sentMessage(#selector(UINavigationControllerDelegate.navigationController(_:didShow:animated:)))
+        .map { _ in }
+        .bind(to: subject)
+      
+      guard navigationController.popViewController(animated: animated) != nil else {
+        fatalError("Failed to pop \(self.currentViewController) from \(navigationController)")
+      }
+      
+      currentViewController = SceneCoordinator.actualViewController(for: navigationController.viewControllers.last!)
+    } else {
+      // View controller has nothing to Pop
+      fatalError("SceneCoordinator.pop(animated:) had nothing to pop when called from \(currentViewController)")
+    }
+    
+    return subject.asObservable()
+      .take(1)
+      .ignoreElements()
+    
   }
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
